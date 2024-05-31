c8e598dcc1a28758e1f891f1488baba86157afbf
alexey_zubkov <zubkov.superstar@yandex.ru>
2024-05-31 16:30:04 +0300

    Добавлен файл utils.py с функциями reading_file_json и completed_operations

---
+++ cp_3/utils.py
@@ -0,0 +1,27 @@
+import json
+from config import PATH_OPERATIONS
+
+
+def reading_file_json(operations):
+    """
+    Чтение файла формата .json
+    """
+    with open(operations, "r", encoding="utf-8") as file:
+        file_json = json.loads(file.read())
+        return file_json
+
+
+def completed_operations():
+    """
+    Возвращает список всех выполненых операций
+    """
+    operations_list = reading_file_json(PATH_OPERATIONS)
+    list_completed_operations = []
+    for opera in operations_list:
+        if opera.get("state") is None:
+            continue
+        elif opera["state"] == "EXECUTED":
+            list_completed_operations.append(opera)
+    return list_completed_operations
+
+
