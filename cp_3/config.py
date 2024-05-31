9fadf429adb18cee829901b72dd59fc4a97008a5
alexey_zubkov <zubkov.superstar@yandex.ru>
2024-05-31 16:34:54 +0300

    Добавлено описание проекта в файла README.md. В файл pyproject.toml добавилась библиотека pathlib В файл .gitignore добавил директории: .DS_Store, __pycache__ В файле config.py добавились пути проекта

---
+++ cp_3/config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+# Путь коренной папки программы
+ROOT_PATH = Path(__file__).parent.parent
+# Путь до дериктории "cp_3"
+PATH_CP_3 = ROOT_PATH.joinpath("cp_3")
+# Путь до файлу "operations.json"
+PATH_OPERATIONS = PATH_CP_3.joinpath("operations.json")
+
