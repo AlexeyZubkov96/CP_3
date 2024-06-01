5f42f74da75459f07bdaa74fc0df94ba9b159095
alexey_zubkov <zubkov.superstar@yandex.ru>
2024-06-01 20:32:07 +0300

    Изминения в файле main.py

--- cp_3/main.py
+++ cp_3/main.py
@@ -1,9 +1,16 @@
-from utils import correct_dictionary
+from utils import reading_file_json, correct_dictionary, completed_operations, last_five_operations, sorting_by_date, date_formation
+from config import PATH_OPERATIONS, count_operations
 
 
 def main():
-    correct = correct_dictionary()
-    for cor in correct:
+    file_json_convert = reading_file_json(PATH_OPERATIONS)
+    execute_operations = completed_operations(file_json_convert)
+    five_execute_operations = last_five_operations(execute_operations, count_operations)
+    sorted_date = sorting_by_date(five_execute_operations)
+    print(sorted_date)
+    correct_print = correct_dictionary(sorted_date)
+    print(correct_print)
+    for cor in correct_print:
         date_ = cor["date"]
         operation = cor["operation"]
         amount = cor["amount"]
