a2c35327c4a9a0e05913d700fdd5750de311d326
alexey_zubkov <zubkov.superstar@yandex.ru>
2024-06-04 21:29:00 +0300

    В файл main.py добавлен запрос у пользователя для количества нужных операций

--- cp_3/main.py
+++ cp_3/main.py
@@ -1,13 +1,14 @@
 from cp_3.utils import reading_file_json, correct_dictionary, completed_operations, last_five_operations, sorting_by_date
-from cp_3.config import PATH_OPERATIONS, count_operations
+from cp_3.config import PATH_OPERATIONS
 
 
 def main():
+    count_operations_user = int(input("Укажите нужное колличство последних выполненых операций: \n"))
     file_json_convert = reading_file_json(PATH_OPERATIONS)
     execute_operations = completed_operations(file_json_convert)
-    five_execute_operations = last_five_operations(execute_operations, count_operations)
-    sorted_date = sorting_by_date(five_execute_operations)
-    correct_print = correct_dictionary(sorted_date)
+    sorted_date = sorting_by_date(execute_operations)
+    five_execute_operations = last_five_operations(sorted_date, count_operations_user)
+    correct_print = correct_dictionary(five_execute_operations)
     result = ""
     for cor in correct_print:
         date_ = cor["date"]
